ff9a4fa64a45346e6ee5fe3db74fe286fd3ba552
Sebastian Godoy Rubio <s.godoy.rubio@gmail.com>
2021-06-23 19:04:08 -0400

    Prueba Strappi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '09e91386894c5769820a4a259e790904'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '34d1ee66-08ad-44a5-8536-9cd9946bd228'
+};
